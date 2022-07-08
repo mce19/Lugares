7dd7e7135643f6bb0c8116c656ad9f7df6fed6cc
Maykol castro <maycolcastroesp19@gmail.com>
2022-07-08 14:01:46 -0600

    semana 8 firestoreCloud

--- app/src/main/java/com/lugares/data/LugarDao.kt
+++ app/src/main/java/com/lugares/data/LugarDao.kt
@@ -1,21 +1,38 @@
 package com.lugares_v.data
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.room.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.lugares_v.model.Lugar
 
-@Dao
-interface LugarDao {
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun addLugar(lugar: Lugar)
+class LugarDao {
 
-    @Update(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun updateLugar(lugar: Lugar)
+    private val coleecion1 = "lugaresApp"
+    private val usuario = Firebase.auth.currentUser?.email.toString()
+    private val coleecion2 = "misLugares"
 
-    @Delete
-    suspend fun deleteLugar(lugar: Lugar)
 
-    @Query ("SELECT * FROM LUGAR")
-    fun getAllData() : LiveData<List<Lugar>>
+
+
+}
+
+
+fun getAllData(): MutableLiveData<list<Lugar>>{
+
+    val listLugares = MutableLiveData<list<Lugar>>()
+    firestore.collection(coleccion1)
+
+}
+
+fun saveLugar(lugar: lugar){
+
+
+}
+
+fun deleteLugar(lugar: lugar){
+
+
 }
